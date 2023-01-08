57412374104b6286249ecc1341d784104f2a7907
courfedra <franandres93@gmail.com>
2023-01-08 15:05:51 -0300

    Add Font and remove sticky title

--- src/components/SubTitulo.jsx
+++ src/components/SubTitulo.jsx
@@ -2,15 +2,12 @@ import styled from "styled-components"
 import Theme from "./Themes"
 
 const H2SubTitulo=styled.h2`
-    width: 50vw;
-    min-height: 10vh;
-    position: sticky;
-    top: 0;
+    width: 100%;
+    min-height: 25vh;
     display: flex;
     justify-content: center;
     align-items: center;
-    letter-spacing: 2.5px;
-    border-radius: 15px;
+    letter-spacing: 5px;
     background-color: ${Theme.colorBg};
     color: ${Theme.colorTitulo};
     
